0d7c7f4905168b3b41ead4a0f2070462094a4999
Elda-Dani <eldadani2001@gmail.com>
2022-10-07 16:04:08 +0530

    route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,20 @@ import { AppComponent } from './app.component';
 import { AddstudentComponent } from './addstudent/addstudent.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchstudentComponent } from './searchstudent/searchstudent.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes:Routes=[
+
+{
+
+  path:"",component:AddstudentComponent
+},
+{
+  path:"search",component:SearchstudentComponent
+}
+]
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +30,8 @@ import { SearchstudentComponent } from './searchstudent/searchstudent.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
