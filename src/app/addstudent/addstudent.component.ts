bd5f2e57289db3d974248426d2adb29ae38dd6f9
Elda-Dani <eldadani2001@gmail.com>
2022-10-13 23:28:49 +0530

    ss

--- src/app/addstudent/addstudent.component.ts
+++ src/app/addstudent/addstudent.component.ts
@@ -9,6 +9,37 @@ export class AddstudentComponent implements OnInit {
 
   constructor() { }
 
+name=""
+rollNo=""
+college=""
+admissionNo=""
+parentName=""
+dob=""
+address=""
+mobile=""
+parentMobile=""
+emailId=""
+bloodGroup=""
+
+readValue=()=>{
+  let data={
+    "name":this.name,
+    "rollNo":this.rollNo,
+    "college":this.college,
+    "admissionNo":this.admissionNo,
+    "parentName":this.parentName,
+    "dob":this.dob,
+    "address":this.address,
+    "mobile":this.mobile,
+    "parentMobile":this.parentMobile,
+    "emailId":this.emailId,
+    "bloodGroup":this.bloodGroup
+  }
+  console.log(data)
+}
+
+
+
   ngOnInit(): void {
   }
 
